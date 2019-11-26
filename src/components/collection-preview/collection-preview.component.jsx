27c340cfc7d8570b10a90dd9428edb56e908f5c4
HeideQueen <lucyfabz@gmail.com>
2019-11-25 20:09:16 -0600

    added redux management for the shopping cart items

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -8,9 +8,7 @@ const CollectionPreview = ({ title, items }) => (
 	<div className="collection-preview">
 		<h1 className="title">{title.toUpperCase()}</h1>
 		<div className="preview">
-			{items
-				.filter((item, i) => i < 4)
-				.map(({ id, ...itemsProps }) => <CollectionItem key={id} {...itemsProps} />)}
+			{items.filter((item, i) => i < 4).map((item) => <CollectionItem key={item.id} item={item} />)}
 		</div>
 	</div>
 );
