068fb37479a89df7abba983ab850e52b0b771bbd
HeideQueen <lucyfabz@gmail.com>
2019-11-26 20:16:58 -0600

    implement reselect to select state

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 
 import './cart-dropdown.styles.scss';
 
+import { selectCartItems } from '../../redux/cart/cart.selectors';
+
 import CustomButton from '../custom-button/custom-button.component';
 import CartItem from '../cart-item/cart-item.component';
 
@@ -13,8 +15,8 @@ const CartDropdown = ({ cartItems }) => (
 	</div>
 );
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-	cartItems
+const mapStateToProps = (state) => ({
+	cartItems: selectCartItems(state)
 });
 
 export default connect(mapStateToProps)(CartDropdown);
