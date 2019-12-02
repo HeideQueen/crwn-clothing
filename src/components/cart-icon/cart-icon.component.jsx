86a2badd9492f5f6e997b793fd3d18dd6381b75c
HeideQueen <lucyfabz@gmail.com>
2019-12-01 22:03:42 -0600

    Add some more styled-components conversions

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
+import React from "react";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
-import { toggleCartHidden } from '../../redux/cart/cart.actions';
-import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
+import { toggleCartHidden } from "../../redux/cart/cart.actions";
+import { selectCartItemsCount } from "../../redux/cart/cart.selectors";
 
-import './cart-icon.styles.scss';
-import { ReactComponent as ShoppingIcon } from '../../assets/shopping-bag.svg';
+import {
+  CartIconContainer,
+  IconContainer,
+  ItemCount
+} from "./cart-icon.styles";
 
 const CartIcon = ({ toggleCartHidden, itemCount }) => (
-	<div className="cart-icon" onClick={toggleCartHidden}>
-		<ShoppingIcon className="shopping-icon" />
-		<span className="item-count">{itemCount}</span>
-	</div>
+  <CartIconContainer onClick={toggleCartHidden}>
+    <IconContainer className="shopping-icon" />
+    <ItemCount>{itemCount}</ItemCount>
+  </CartIconContainer>
 );
 
 const mapStateToProps = createStructuredSelector({
-	itemCount: selectCartItemsCount
+  itemCount: selectCartItemsCount
 });
 
-const mapDispatchToProps = (dispatch) => ({
-	toggleCartHidden: () => dispatch(toggleCartHidden())
+const mapDispatchToProps = dispatch => ({
+  toggleCartHidden: () => dispatch(toggleCartHidden())
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CartIcon);
--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -1,17 +1,22 @@
-import React from 'react';
+import React from "react";
 
-import './cart-item.styles.scss';
+import {
+  CartItemContainer,
+  Image,
+  ItemDetails,
+  Name
+} from "./cart-item.styles";
 
 const CartItem = ({ item: { imageUrl, price, name, quantity } }) => (
-	<div className="cart-item">
-		<img src={imageUrl} alt="item" />
-		<div className="item-details">
-			<span className="name">{name}</span>
-			<span className="price">
-				{quantity} x ${price}
-			</span>
-		</div>
-	</div>
+  <CartItemContainer>
+    <Image src={imageUrl} alt="item" />
+    <ItemDetails>
+      <Name>{name}</Name>
+      <span className="price">
+        {quantity} x ${price}
+      </span>
+    </ItemDetails>
+  </CartItemContainer>
 );
 
 export default CartItem;
